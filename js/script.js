270bd50b4a226dd7e98cea025acedfa49a0edc87
AndreasVilhelmsson <addevilhelmsson@hotmail.com>
2021-09-16 08:19:42 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,50 @@
+
+/*********** toggle button for navbar ********************** */
 let menu = document.querySelector("#menu-bars");
 let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
   menu.classList.toggle("fa-times");
   navbar.classList.toggle("active");
-  console.log("hello");
 };
+
+/****** navbar color change using Intersection Observer API
+The Intersection Observer API lets code register a callback
+function that is executed whenever an element they
+wish to monitor enters or exits another element (or the viewport),
+*/
+
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".main-page");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.map(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("scrollbar");
+    } else {
+      header.classList.remove("scrollbar");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+const str = document.querySelector('input');
+str.addEventListener('input', () => {
+
+})
+function checkValidEmailAdress(str){
+
+}
+
+
+
+
